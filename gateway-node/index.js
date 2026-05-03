5c90fded7cd022bb300f3f7fa4f8dfa790459f4e
maahimair <maahimair@gmail.com>
2026-05-03 18:55:12 +0530

    Day 3: Connected gateway with backend API

--- gateway-node/index.js
+++ gateway-node/index.js
@@ -7,6 +7,29 @@ app.get("/", (req, res) => {
   res.send("Fractal Vault Gateway Running");
 });
 
+app.post("/check-trust", async (req, res) => {
+  try {
+    const response = await fetch("http://127.0.0.1:5000/evaluate-trust", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    const data = await response.json();
+
+    res.json({
+      gateway: "Node.js Gateway",
+      backend_response: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Python backend not reachable"
+    });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Gateway running on port 3000");
 });
